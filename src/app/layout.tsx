fb23f6351ed6b0013911514d36684c8a3efc1d92
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2023-09-29 18:28:48 -0300

    fix(alias): resolvendo o problema na importação dos alias dos providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 import './globals.css'
 import { Roboto } from 'next/font/google'
-import NextAuthProviderSession from '@/providers/sessionProviders'
+import NextAuthProviderSession from '@providers/sessionProviders'
 
 const roboto = Roboto({
   subsets: ['latin'],

e00d7c77d50180d45dee94a0a71979868915f37e
Gabriel Silva <contato.gabrieljosesilva@gmail.com>
2023-09-29 18:37:48 -0300

    chore(sidebar): criando e estilizando a SideBar

---
+++ src/providers/sideBarProvider.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import SideBar from '@/components/shared/sideBar/sideBar'
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useState,
+} from 'react'
+
+interface setSideBarActivatedState {
+  sideBarActivated: boolean
+  setSideBarActivated: Dispatch<SetStateAction<boolean>>
+  toggleOpenSideBar: () => void
+}
+
+export const SideBarContext = createContext({} as setSideBarActivatedState)
+
+const SideBarProvider = ({ children }: { children: ReactNode }) => {
+  const [sideBarActivated, setSideBarActivated] = useState<boolean>(false)
+
+  const toggleOpenSideBar = () => {
+    setSideBarActivated((prevState) => !prevState)
+  }
+  return (
+    <SideBarContext.Provider
+      value={{ sideBarActivated, setSideBarActivated, toggleOpenSideBar }}
+    >
+      <SideBar>{children}</SideBar>
+    </SideBarContext.Provider>
+  )
+}
+
+export default SideBarProvider
